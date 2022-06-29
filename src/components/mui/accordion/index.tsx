82d2f9e66de14f3f1411ed4f397a965ae80efb4d
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-06-29 18:13:55 +0700

    update accordion productpage, import card to productpage, styling mui accordion

--- src/components/mui/accordion/index.tsx
+++ src/components/mui/accordion/index.tsx
@@ -7,45 +7,33 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function SimpleAccordion() {
   return (
-    <div>
+    <div className='w-[241px]'>
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography>Price</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+            <div className='space-x-2'>
+                <input type="checkbox" />
+                <label htmlFor="">Test</label>
+            </div>
+            <div className='space-x-2'>
+                <input type="checkbox" />
+                <label htmlFor="">Test</label>
+            </div>
+            <div className='space-x-2'>
+                <input type="checkbox" />
+                <label htmlFor="">Test</label>
+            </div>
+            <div className='space-x-2'>
+                <input type="checkbox" />
+                <label htmlFor="">Test</label>
+            </div>
         </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
       </Accordion>
     </div>
   );
--- src/containers/product/index.tsx
+++ src/containers/product/index.tsx
@@ -1,6 +1,7 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import BasicBreadcrumbs from "src/components/mui/breadcrumb";
 import SimpleAccordion from "src/components/mui/accordion";
+import Card from "src/components/core/cards";
 
 const ProductPage = () => {
   return (
@@ -10,16 +11,34 @@ const ProductPage = () => {
           <title>Product</title>
         </Helmet>
       </HelmetProvider>
-      <div className="w-full max-w-[1020px] mx-auto">
+      <div className="w-full px-8 max-w-[1084px] mx-auto">
         <div className="py-7">
           <BasicBreadcrumbs />
         </div>
-        <div className="flex">
+        <div className="flex space-x-[18px]">
             <div>
+                <SimpleAccordion />
+                <SimpleAccordion />
+                <SimpleAccordion />
+                <SimpleAccordion />
                 <SimpleAccordion />
             </div>
             <div>
                 <img src="/images/banners/banner_product.png" alt="" />
+                <div>
+                    <div className="flex justify-between">
+                    <p>Result:122</p>
+                    <p>Sort by</p>
+                    </div>
+                    <div className="grid grid-cols-3 gap-[19px]">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    </div>
+                </div>
             </div>
         </div>
       </div>
